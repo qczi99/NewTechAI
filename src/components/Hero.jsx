050b9d452be4fab28b4eb1dd0dcabd296079d7c0
Krystian <kkuczak99@onet.pl>
2024-10-22 21:52:00 +0200

    zmiana hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ return(
     <button><h1>Motoryzacja</h1></button>
   </div>
   <div>
-    <button><h1>IT</h1></button>
+    <button><h1>IT xd</h1></button>
     <button><h1>Biznes</h1></button>
   </div>
 </div>
