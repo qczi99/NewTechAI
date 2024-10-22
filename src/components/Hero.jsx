a6f7a95368428153a724eabdaf3433949d22474d
Krystian <kkuczak99@onet.pl>
2024-10-22 21:31:40 +0200

    Inicialize

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const HeroSection = () => {
+
+return(
+<>
+<div className="hero">
+  <div>
+    <button><h1>Sport</h1></button>
+    <button><h1>Motoryzacja</h1></button>
+  </div>
+  <div>
+    <button><h1>IT</h1></button>
+    <button><h1>Biznes</h1></button>
+  </div>
+</div>
+</>
+)
+}
+
+export default HeroSection;
