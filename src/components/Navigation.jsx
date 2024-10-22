c42df41b490dd57b0fa7b859a0b86f5ac6a0b5b9
KK <83357920+qczi99@users.noreply.github.com>
2024-10-22 21:48:17 +0200

    Add files via upload

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () => {
+
+    return(
+        <>
+        <div>
+          <nav>
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/11/43/tux-158547_1280.png" alt="logo" />
+            <ul>
+              <li href="#">Menu</li>
+              <li href="#">Menu</li>
+              <li href="#">Menu</li>
+              <li href="#">Menu</li>
+            </ul>
+            <button>Login</button>
+          </nav>
+        </div>
+      </>
+      )
+
+}
+
+export default Navigation;
